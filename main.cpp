717d25e080ea435bb305e791e62891682942546e
Samuel Rowe <samuelrowe1999@gmail.com>
2020-03-10 20:24:27 +0530

    Fixed a bazillion syntax errors. C++ you beauty! :)

--- main.cpp
+++ main.cpp
@@ -1,45 +1,52 @@
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 #include <jaegertracing/Tracer.h>
-
-using namespace std;
-using namespace opentracing;
-using namespace jaegertracing::logging;
-using namespace YAML;
+#include <string>
 
 void setUpTracer(const char* path) {
-	auto file = LoadFile(path);
-	auto config = Config::parse(file);
-	auto logger = consoleLogger();
-	auto tracer = Tracer::make("service", config, logger);
-	Tracer::InitGlobal(static_pointer_cast<Tracer>(tracer));
+	auto file = YAML::LoadFile(path);
+	auto config = jaegertracing::Config::parse(file);
+	auto tracer = jaegertracing::Tracer::make("service", config, jaegertracing::logging::consoleLogger());
+	opentracing::Tracer::InitGlobal(std::static_pointer_cast<opentracing::Tracer>(tracer));
 
-	cout << "[info] The tracer was successfully initialized.\n";
+	std::cout << "[info] The tracer was successfully initialized.\n";
 }
 
 void shutdownTracer() {
-	auto globalTracer = Tracer::Global();
+	auto globalTracer = opentracing::Tracer::Global();
 	globalTracer->Close();
 
-	cout << "[info] The tracer was successfully shutdown.\n";
+	std::cout << "[info] The tracer was successfully shutdown.\n";
 }
 
-unique_ptr<Span> addSpan(const char* name) {
-	auto globalTracer = Tracer::Global();
+std::unique_ptr<opentracing::Span> addSpan(const std::string& name) {
+	auto globalTracer = opentracing::Tracer::Global();
 	auto span = globalTracer->StartSpan(name);
 
 	return span;
 }
 
-unique_ptr<Span> addChildSpan(const unique_ptr<Span>& parent, const char* name) {
-	auto globalTracer = Tracer::Global();
-	auto parentContext = parent->Context();
-	auto child = ChildOf(parentContext);
+std::unique_ptr<opentracing::Span> addChildSpan(const std::unique_ptr<opentracing::Span>& parent, const std::string& name) {
+	auto globalTracer = opentracing::Tracer::Global();
+	auto parentContext = &parent->context();
+	auto child = opentracing::ChildOf(parentContext);
 	auto span = globalTracer->StartSpan(name, { child });
 
-	cout << "[info] Successfully created tracer named '" << name << "'.\n";
+	std::cout << "[info] Successfully created tracer named '" << name << "'.\n";
 
 	return span;
+}
+
+void function2(const std::unique_ptr<opentracing::Span>& parent) {
+	auto span = addChildSpan(parent, "function2");
+
+	std::cout << "[info] function2 done.\n";
+}
+
+void function3(const std::unique_ptr<opentracing::Span>& parent) {
+	auto span = addChildSpan(parent, "function3");
+
+	std::cout << "[info] function3 done.\n";
 }
 
 void function1() {
@@ -48,19 +55,7 @@ void function1() {
 	function2(span);
 	function3(span);
 
-	cout << "[info] function1 done.\n";
-}
-
-void function2(const std::unique_ptr<Span>& parent) {
-	auto span = addChildSpan(parent, "function2");
-
-	cout << "[info] function2 done.\n";
-}
-
-void function3(const std::unique_ptr<Span>& parent) {
-	auto span = addChildSpan(parent, "function3");
-
-	cout << "[info] function3 done.\n";
+	std::cout << "[info] function1 done.\n";
 }
 
 void function4() {
@@ -71,13 +66,13 @@ void function4() {
 	function2(span);
 	function3(span);
 
-	cout << "[info] function4 done.\n";
+	std::cout << "[info] function4 done.\n";
 }
 
 int main(int length, char** arguments) {
 	int result = 0;
 	if (length < 2) {
-		cerr < "[error] Please specify a configuration file.\n";
+		std::cout << "[error] Please specify a configuration file.\n";
 		result = 1;
 	}
 	else {
